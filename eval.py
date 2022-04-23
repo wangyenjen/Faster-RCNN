5e5a39c9cbecc6c4d51486b1a861a18cfb526289
Yen-Jen Wang <wangyenjen0301@gmail.com>
2022-04-23 16:44:17 +0800

    Add some rough comments for eval.py

--- eval.py
+++ eval.py
@@ -52,29 +52,32 @@ def fasterrcnn_eval(dataset_path, ckpt_path, ann_file):
 
     print("Start create dataset!", flush=True)
 
-    # When create MindDataset, using the fitst mindrecord file, such as FasterRcnn.mindrecord0.
+    # === 1. Create dataset for inference and evaluation. ===
     dataset = create_fasterrcnn_dataset(dataset_path, batch_size=config.test_batch_size,
                                         device_num=device_num, rank_id=rank,
                                         num_parallel_workers=config.num_parallel_workers,
                                         python_multiprocessing=config.python_multiprocessing,
                                         is_training=False)
-
+    dataset_coco = COCO(ann_file)
     dataset_size = dataset.get_dataset_size()
     print("Create dataset done!", flush=True)
+    # ======================================================
 
+
+    # ================== 2. Load model. ====================
     net = Faster_Rcnn_Resnet50(config=config)
 
     load_path = ckpt_path
     param_dict = load_checkpoint(load_path)
     load_param_into_net(net, param_dict)
 
-
     net = net.set_train(False)
+    # ======================================================
 
+
+    # === For each batch, generate the bbox for each single image. ===
     eval_iter = 0
     outputs = []
-    dataset_coco = COCO(ann_file)
-
     max_num = 128
     for data in dataset.create_dict_iterator(num_epochs=1):
         eval_iter += 1
@@ -99,9 +102,14 @@ def fasterrcnn_eval(dataset_path, ckpt_path, ann_file):
 
             output_1image = bbox2result_1image(res_bbox, res_label, config.num_classes)
             outputs.append(output_1image)
+    # =================================================================
 
+    
+
+    # === 4. Store the result as json format and evaluate the performance. ===
     result_files = results2json(dataset_coco, outputs, "./output")
     coco_eval(result_files, ["bbox"], dataset_coco, single_result=True)
+    # ========================================================================
 
 
 if __name__ == '__main__':
